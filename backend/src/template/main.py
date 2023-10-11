346e7f73a35d5b7483bd472ed9f92aca4f43aff2
Nassir Al-Khishman <khishmann@gmail.com>
2023-10-10 22:33:41 -0400

    Allow CORS

--- backend/src/template/main.py
+++ backend/src/template/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# allow CORS for all origins on all routes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
